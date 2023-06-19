e6cacdcbc120623c374cfebed80aa214cf66ab97
Paula <paulasalinor@gmail.com>
2023-06-19 09:04:01 -0300

    add google analitycs

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,5 @@
 import Document, { DocumentContext, DocumentInitialProps, Html, Head, Main, NextScript  } from 'next/document';
- 
+ import Script from "next/script";
 class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext,
@@ -15,6 +15,19 @@ class MyDocument extends Document {
         <Head lang='pt-BR'>
           <title>Paula Ribeiro | Blog</title>
           <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
+          <Script 
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            strategy="afterInteractive"
+          />
+          <Script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+          `}
+          </Script>
         </Head>
         <body>
           <Main />
