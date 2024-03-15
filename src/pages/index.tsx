2fae4d8d02120f1da7c55ce13bfeb8d5479b3207
Paula Salino Ribeiro <paula.ribeiro@easylive.com.br>
2024-03-15 11:19:30 -0300

    fix: order posts by date

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,38 @@ type Props = {
   posts: PostContent[];
 };
 
+
+function parseDateFromString(dateString) {
+    const monthMap = {
+        "Janeiro": 0,
+        "Fevereiro": 1,
+        "Março": 2,
+        "Abril": 3,
+        "Maio": 4,
+        "Junho": 5,
+        "Julho": 6,
+        "Agosto": 7,
+        "Setembro": 8,
+        "Outubro": 9,
+        "Novembro": 10,
+        "Dezembro": 11
+    };
+
+    const parts = dateString.split(' ');
+
+    const day = parseInt(parts[0]);
+    const month = monthMap[parts[2]];
+    const year = parseInt(parts[4]);
+
+    const date = new Date(year, month, day);
+
+    return date;
+}
+
 export default function Home( { posts }: Props ) {
+  // @ts-ignore
+  posts.sort((a, b) => parseDateFromString(b.date) - parseDateFromString(a.date))
+  
   return (
     <div className="container">
       <Profile path="/images/profile.png"/>
